1f867a1be8011343dba6df4d6dd11c9b041afec4
Habibov97 <najaff.habibov@gmail.com>
2026-06-16 04:20:41 +0400

    cors policies update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({});
+  app.enableCors({
+    origin: [, 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useGlobalPipes(
