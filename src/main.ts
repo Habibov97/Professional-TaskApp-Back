ca00c3a5724b28fec7c0964829eddfc14ce777b7
Habibov97 <najaff.habibov@gmail.com>
2026-06-29 23:25:57 +0400

    cors policy updated

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://prodessional-task-app-front.vercel.app',
+    ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   });
   app.setGlobalPrefix('api');
   app.use(cookieParser());
